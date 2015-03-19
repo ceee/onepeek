1fe2db2f6b1ce27f109ee95f0166884c15d280ff
Tobias Klika <cee@live.at>
2015-03-19 22:19:19 +0100

    create solution

---
+++ src/OnePeek.Api.Spotlight/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace OnePeek
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            Console.WriteLine("onepeek");
+            Console.ReadLine();
+        }
+    }
+}
