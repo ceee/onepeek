15e5fa885860be450dfb87abbeb25bae4434e076
Tobias Klika <cee@live.at>
2015-03-23 23:22:03 +0100

    start http client

---
+++ src/OnePeek.Api/ApiHttpClient.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace OnePeek.Api
+{
+  /// <summary>
+  /// Wrapper for the HttpClient
+  /// </summary>
+  internal class ApiHttpClient : IDisposable
+  {
+    internal static ApiHttpClient _instance = null;
+
+    /// <summary>
+    /// Get Singleton of ApiHttpCient
+    /// </summary>
+    internal static ApiHttpClient Instance { get { return _instance ?? (_instance = new ApiHttpClient()); } }
+
+    /// <summary>
+    /// Client for HTTP communication
+    /// </summary>
+    protected readonly HttpClient _restClient;
+
+
+    public ApiHttpClient()
+    {
+      HttpClient = new HttpClient(handler ?? new HttpClientHandler()
+      {
+        AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip
+      });
+    }
+
+
+    public void Dispose()
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
