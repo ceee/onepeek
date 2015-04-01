cbc47bd779cd7c64dafe6b5cdba630e63324d336
Tobias Klika <cee@live.at>
2015-04-01 02:15:02 +0200

    AppImage extension method to get the uri

---
+++ src/OnePeek.Api/Extensions/EntityExtensions.cs
@@ -0,0 +1,17 @@
+﻿using OnePeek.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnePeek.Api.Extensions
+{
+  public static class EntityExtensions
+  {
+    public static Uri GetUri(this AppImage appImage, StoreImageType imageType = StoreImageType.None)
+    {
+      return EndpointUris.GetWindowsPhoneImageUri(appImage.Urn, imageType.GetEnumDisplayName());
+    }
+  }
+}
