5a313c1d61e98a48fc7f1186b46f246c155a6b78
Tobias Klika <cee@live.at>
2015-04-03 22:40:12 +0200

    add test frontend (WebConsole)

--- src/OnePeek.WebConsole/Modules/ApiModule.cs
+++ src/OnePeek.WebConsole/Modules/ApiModule.cs
@@ -3,7 +3,7 @@ using OnePeek.Api;
 using OnePeek.Entities;
 using System.IO;
 
-namespace OnePeek.WebConsole
+namespace OnePeek.WebConsole.Modules
 {
   public class ApiModule : NancyModule
   {
@@ -12,22 +12,21 @@ namespace OnePeek.WebConsole
       AppRatingEndpoint ratingEndpoint = new AppRatingEndpoint();
       AppMetadataEndpoint metaEndpoint = new AppMetadataEndpoint();
 
-
-      Get["/meta/{id?2532ff45-aa3f-4aba-a266-ed7ec71d47bd}", true] = async (ctx, token) =>
+      Get["/meta/{id}", true] = async (ctx, token) =>
       {
         AppMetadata meta = await metaEndpoint.GetMetadata(ctx.id, StoreType.WindowsPhone8, StoreCultureType.EN_US);
         return Response.AsJson(meta);
       };
 
 
-      Get["/reviews/{id?2532ff45-aa3f-4aba-a266-ed7ec71d47bd}", true] = async (ctx, token) =>
+      Get["/reviews/{id}", true] = async (ctx, token) =>
       {      
         AppReviews reviews = await ratingEndpoint.GetReviews(ctx.id, StoreType.WindowsPhone8, StoreCultureType.EN_US, StoreReviewSorting.Latest);
         return Response.AsJson(reviews);
       };
 
 
-      Get["/image/{id?4274cebb-01c7-4788-97f7-635322fa4877}", true] = async (ctx, token) =>
+      Get["/image/{id}.jpg", true] = async (ctx, token) =>
       {
         Stream imageStream = await metaEndpoint.GetImageAsStream(ctx.id, StoreImageType.None);
         return Response.FromStream(imageStream, "image/jpeg");
---
+++ src/OnePeek.WebConsole/Modules/HomeModule.cs
@@ -0,0 +1,35 @@
+﻿using Nancy;
+using OnePeek.Api;
+using OnePeek.Entities;
+using System.IO;
+
+namespace OnePeek.WebConsole.Modules
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      Configuration.UseFiveStarSystem = true;
+      AppRatingEndpoint ratingEndpoint = new AppRatingEndpoint();
+      AppMetadataEndpoint metaEndpoint = new AppMetadataEndpoint();
+
+      Get["/"] = ctx =>
+      {
+        return View["Index"];
+      };
+
+      Get["/meta", true] = async (ctx, token) =>
+      {
+        AppMetadata meta = await metaEndpoint.GetMetadata(Request.Query["id"], StoreType.WindowsPhone8, StoreCultureType.EN_US);
+        return View["Meta", meta];
+      };
+
+
+      Get["/reviews", true] = async (ctx, token) =>
+      {
+        AppReviews reviews = await ratingEndpoint.GetReviews(Request.Query["id"], StoreType.WindowsPhone8, StoreCultureType.EN_US, StoreReviewSorting.Latest);
+        return View["Reviews", reviews];
+      };
+    }
+  }
+}
