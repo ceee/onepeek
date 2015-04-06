585890e37b8958a4414851f028a491a9f2f072b1
Tobias Klika <cee@live.at>
2015-04-07 00:41:12 +0200

    docs for AppImage

--- src/OnePeek.Entities/AppImage.cs
+++ src/OnePeek.Entities/AppImage.cs
@@ -4,13 +4,26 @@ using System.Linq;
 
 namespace OnePeek.Entities
 {
+  /// <summary>
+  /// An image contains the ID which is necessary to get an appropriate URL for the image or the stream
+  /// see OnePeek.Api.Extensions
+  /// </summary>
   public partial class AppImage
   {
+    /// <summary>
+    /// ID.
+    /// </summary>
     public string Id { get { return !String.IsNullOrWhiteSpace(Urn) ? Urn.Split(':').LastOrDefault() : null; } }
 
+    /// <summary>
+    /// URN
+    /// </summary>
     [XmlElement("id")]
     public string Urn { get; set; }
 
+    /// <summary>
+    /// Rotation angle (is 0, 90, 180 or 270)
+    /// </summary>
     public short Rotation { get; set; }
   }
 }
