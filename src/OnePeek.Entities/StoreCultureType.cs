8d5b204b04008be479cc2060401d4d42db616abc
Tobias Klika <cee@live.at>
2015-04-11 22:30:40 +0200

    add all cultures to enum

--- src/OnePeek.Entities/StoreCultureType.cs
+++ src/OnePeek.Entities/StoreCultureType.cs
@@ -2,9 +2,521 @@
 {
   public enum StoreCultureType
   {
+    /// <summary>
+    /// Undefined culture, will throw in API
+    /// </summary>
     Unknown,
-    EN_US,
+    /// <summary>
+    /// Angola (Português - Portugal)
+    /// </summary>
+    PT_AO,
+    /// <summary>
+    /// Argentina (Español)
+    /// </summary>
+    ES_AR,
+    /// <summary>
+    /// Armenia (English)
+    /// </summary>
+    EN_AM,
+    /// <summary>
+    /// Australia (English)
+    /// </summary>
+    EN_AU,
+    /// <summary>
+    /// Azərbaycan (Azərbaycan)
+    /// </summary>
+    AZ_LATN_AZ,
+    /// <summary>
+    /// Bangladesh (English)
+    /// </summary>
+    EN_BD,
+    /// <summary>
+    /// België (Nederlands)
+    /// </summary>
+    NL_BE,
+    /// <summary>
+    /// Belgique (français)
+    /// </summary>
+    FR_BE,
+    /// <summary>
+    /// Bénin (français)
+    /// </summary>
+    FR_BJ,
+    /// <summary>
+    /// Bolivia (Español)
+    /// </summary>
+    ES_BO,
+    /// <summary>
+    /// Brasil (Português - Brasil)
+    /// </summary>
+    PT_BR,
+    /// <summary>
+    /// Burkina Faso (français)
+    /// </summary>
+    FR_BF,
+    /// <summary>
+    /// Burundi (français)
+    /// </summary>
+    FR_BI,
+    /// <summary>
+    /// Cameroon (English)
+    /// </summary>
+    EN_CM,
+    /// <summary>
+    /// Canada (English)
+    /// </summary>
+    EN_CA,
+    /// <summary>
+    /// Canada (français)
+    /// </summary>
+    FR_CA,
+    /// <summary>
+    /// Česká republika (čeština)
+    /// </summary>
+    CS_CZ,
+    /// <summary>
+    /// Chile (Español)
+    /// </summary>
+    ES_CL,
+    /// <summary>
+    /// Colombia (Español)
+    /// </summary>
+    ES_CO,
+    /// <summary>
+    /// Congo (français)
+    /// </summary>
+    FR_CD,
+    /// <summary>
+    /// Costa Rica (Español)
+    /// </summary>
+    ES_CR,
+    /// <summary>
+    /// Côte d'Ivoire (français)
+    /// </summary>
+    FR_CI,
+    /// <summary>
+    /// Danmark (dansk)
+    /// </summary>
+    DA_DK,
+    /// <summary>
+    /// Deutschland (Deutsch)
+    /// </summary>
+    DE_DE,
+    /// <summary>
+    /// Ecuador (Español)
+    /// </summary>
+    ES_EC,
+    /// <summary>
+    /// Eesti (eesti)
+    /// </summary>
+    ET_EE,
+    /// <summary>
+    /// El Salvador (Español)
+    /// </summary>
+    ES_SV,
+    /// <summary>
+    /// España (Español)
+    /// </summary>
+    ES_ES,
+    /// <summary>
+    /// Espanya (Català)
+    /// </summary>
+    CA_ES,
+    /// <summary>
+    /// France (français)
+    /// </summary>
+    FR_FR,
+    /// <summary>
+    /// Ghana (English)
+    /// </summary>
+    EN_GH,
+    /// <summary>
+    /// Guatemala (Español)
+    /// </summary>
+    ES_GT,
+    /// <summary>
+    /// Guinée (français)
+    /// </summary>
+    FR_GN,
+    /// <summary>
+    /// Haïti (français)
+    /// </summary>
+    FR_HT,
+    /// <summary>
+    /// Honduras (Español)
+    /// </summary>
+    ES_HN,
+    /// <summary>
+    /// Hong Kong (English)
+    /// </summary>
+    EN_HK,
+    /// <summary>
+    /// Hrvatska (hrvatski)
+    /// </summary>
+    HR_HR,
+    /// <summary>
+    /// Iceland (English)
+    /// </summary>
+    IS_IS,
+    /// <summary>
+    /// India (English)
+    /// </summary>
+    EN_IN,
+    /// <summary>
+    /// Indonesia (Bahasa Indonesia)
+    /// </summary>
+    ID_ID,
+    /// <summary>
+    /// Ireland (English)
+    /// </summary>
+    EN_IE,
+    /// <summary>
+    /// Italia (italiano)
+    /// </summary>
+    IT_IT,
+    /// <summary>
+    /// Kenya (English)
+    /// </summary>
+    EN_KE,
+    /// <summary>
+    /// Kuwait (English)
+    /// </summary>
+    EN_KW,
+    /// <summary>
+    /// Latvija (latviešu)
+    /// </summary>
+    LV_LV,
+    /// <summary>
+    /// Liechtenstein (Deutsch)
+    /// </summary>
+    DE_LI,
+    /// <summary>
+    /// Lietuva (lietuvių)
+    /// </summary>
+    LT_LT,
+    /// <summary>
+    /// Madagascar (français)
+    /// </summary>
+    FR_MG,
+    /// <summary>
+    /// Magyarország (magyar)
+    /// </summary>
+    HU_HU,
+    /// <summary>
+    /// Malawi (English)
+    /// </summary>
+    EN_MW,
+    /// <summary>
+    /// Malaysia (Bahasa Melayu)
+    /// </summary>
+    MS_MY,
+    /// <summary>
+    /// Malaysia (English)
+    /// </summary>
+    EN_MY,
+    /// <summary>
+    /// Mali (français)
+    /// </summary>
+    FR_ML,
+    /// <summary>
+    /// México (Español)
+    /// </summary>
+    ES_MX,
+    /// <summary>
+    /// Moçambique (Português - Portugal)
+    /// </summary>
+    PT_MZ,
+    /// <summary>
+    /// Nederland (Nederlands)
+    /// </summary>
+    NL_NL,
+    /// <summary>
+    /// New Zealand (English)
+    /// </summary>
+    EN_NZ,
+    /// <summary>
+    /// Nicaragua (Español)
+    /// </summary>
+    ES_NI,
+    /// <summary>
+    /// Niger (français)
+    /// </summary>
+    FR_NE,
+    /// <summary>
+    /// Nigeria (English)
+    /// </summary>
+    EN_NG,
+    /// <summary>
+    /// Norge (norsk)
+    /// </summary>
+    NB_NO,
+    /// <summary>
+    /// Österreich (Deutsch)
+    /// </summary>
     DE_AT,
-    DE_DE
+    /// <summary>
+    /// O'zbekiston (O'zbekcha)
+    /// </summary>
+    UZ_LATN_UZ,
+    /// <summary>
+    /// Pakistan (English)
+    /// </summary>
+    EN_PK,
+    /// <summary>
+    /// Paraguay (Español)
+    /// </summary>
+    ES_PY,
+    /// <summary>
+    /// Perú (Español)
+    /// </summary>
+    ES_PE,
+    /// <summary>
+    /// Philippines (English)
+    /// </summary>
+    EN_PH,
+    /// <summary>
+    /// Pilipinas (Filipino)
+    /// </summary>
+    FIL_PH,
+    /// <summary>
+    /// Polska (polski)
+    /// </summary>
+    PL_PL,
+    /// <summary>
+    /// Portugal (Português - Portugal)
+    /// </summary>
+    PT_PT,
+    /// <summary>
+    /// República Dominicana (Español)
+    /// </summary>
+    ES_DO,
+    /// <summary>
+    /// România (Română)
+    /// </summary>
+    RO_RO,
+    /// <summary>
+    /// Rwanda (français)
+    /// </summary>
+    FR_RW,
+    /// <summary>
+    /// Schweiz (Deutsch)
+    /// </summary>
+    DE_CH,
+    /// <summary>
+    /// Sénégal (français)
+    /// </summary>
+    FR_SN,
+    /// <summary>
+    /// Shqipëria (shqip)
+    /// </summary>
+    SQ_AL,
+    /// <summary>
+    /// Sierra Leone (English)
+    /// </summary>
+    EN_SL,
+    /// <summary>
+    /// Singapore (English)
+    /// </summary>
+    EN_SG,
+    /// <summary>
+    /// Slovenija (slovenščina)
+    /// </summary>
+    SL_SI,
+    /// <summary>
+    /// Slovensko (slovenčina)
+    /// </summary>
+    SK_SK,
+    /// <summary>
+    /// Somalia (English)
+    /// </summary>
+    EN_SO,
+    /// <summary>
+    /// South Africa (English)
+    /// </summary>
+    EN_ZA,
+    /// <summary>
+    /// Srbija (srpski)
+    /// </summary>
+    SR_LATN_CS,
+    /// <summary>
+    /// Suisse (français)
+    /// </summary>
+    FR_CH,
+    /// <summary>
+    /// Suomi (suomi)
+    /// </summary>
+    FI_FI,
+    /// <summary>
+    /// Sverige (svenska)
+    /// </summary>
+    SV_SE,
+    /// <summary>
+    /// Svizzera (italiano)
+    /// </summary>
+    IT_CH,
+    /// <summary>
+    /// Tanzania (English)
+    /// </summary>
+    EN_TZ,
+    /// <summary>
+    /// Tchad (français)
+    /// </summary>
+    FR_TD,
+    /// <summary>
+    /// Togo (français)
+    /// </summary>
+    FR_TG,
+    /// <summary>
+    /// Türkiye (Türkçe)
+    /// </summary>
+    TR_TR,
+    /// <summary>
+    /// Uganda (English)
+    /// </summary>
+    EN_UG,
+    /// <summary>
+    /// United Kingdom (English)
+    /// </summary>
+    EN_GB,
+    /// <summary>
+    /// United States (English)
+    /// </summary>
+    EN_US,
+    /// <summary>
+    /// Venezuela (Español)
+    /// </summary>
+    ES_VE,
+    /// <summary>
+    /// Việt Nam (Tiếng Việt)
+    /// </summary>
+    VI_VN,
+    /// <summary>
+    /// Zambia (English)
+    /// </summary>
+    EN_ZM,
+    /// <summary>
+    /// Zimbabwe (English)
+    /// </summary>
+    EN_ZW,
+    /// <summary>
+    /// Ελλάδα (Ελληνικά)
+    /// </summary>
+    EL_GR,
+    /// <summary>
+    /// Беларусь (Беларуская)
+    /// </summary>
+    BE_BY,
+    /// <summary>
+    /// България (български)
+    /// </summary>
+    BG_BG,
+    /// <summary>
+    /// Қазақстан (Қазақ)
+    /// </summary>
+    KK_KZ,
+    /// <summary>
+    /// Македонија, ПЈР (македонски)
+    /// </summary>
+    MK_MK,
+    /// <summary>
+    /// Россия (Русский)
+    /// </summary>
+    RU_RU,
+    /// <summary>
+    /// Тоҷикистон (Русский)
+    /// </summary>
+    RU_TJ,
+    /// <summary>
+    /// Туркменистан (Русский)
+    /// </summary>
+    RU_TM,
+    /// <summary>
+    /// Україна (українська)
+    /// </summary>
+    UK_UA,
+    /// <summary>
+    /// ישראל (עברית)
+    /// </summary>
+    HE_IL,
+    /// <summary>
+    /// الأردن (العربية)
+    /// </summary>
+    AR_JO,
+    /// <summary>
+    /// الإمارات العربية المتحدة (العربية)
+    /// </summary>
+    AR_AE,
+    /// <summary>
+    /// البحرين (العربية)
+    /// </summary>
+    AR_BH,
+    /// <summary>
+    /// الجزائر (العربية)
+    /// </summary>
+    AR_DZ,
+    /// <summary>
+    /// العراق (العربية)
+    /// </summary>
+    AR_IQ,
+    /// <summary>
+    /// الكويت (العربية)
+    /// </summary>
+    AR_KW,
+    /// <summary>
+    /// المغرب (العربية)
+    /// </summary>
+    AR_MA,
+    /// <summary>
+    /// المملكة العربية السعودية (العربية)
+    /// </summary>
+    AR_SA,
+    /// <summary>
+    /// اليمن (العربية)
+    /// </summary>
+    AR_YE,
+    /// <summary>
+    /// تونس (العربية)
+    /// </summary>
+    AR_TN,
+    /// <summary>
+    /// عمان (العربية)
+    /// </summary>
+    AR_OM,
+    /// <summary>
+    /// قطر (العربية)
+    /// </summary>
+    AR_QA,
+    /// <summary>
+    /// مصر (العربية)
+    /// </summary>
+    AR_EG,
+    /// <summary>
+    /// भारत (हिन्दी)
+    /// </summary>
+    HI_IN,
+    /// <summary>
+    /// ไทย (ไทย)
+    /// </summary>
+    TH_TH,
+    /// <summary>
+    /// 대한민국 (한국어)
+    /// </summary>
+    KO_KR,
+    /// <summary>
+    /// 中国 (简体中文)
+    /// </summary>
+    ZH_CN,
+    /// <summary>
+    /// 台灣 (繁體中文)
+    /// </summary>
+    ZH_TW,
+    /// <summary>
+    /// 日本 (日本語)
+    /// </summary>
+    JA_JP,
+    /// <summary>
+    /// 香港 (繁體中文)
+    /// </summary>
+    ZH_HK
   }
 }
