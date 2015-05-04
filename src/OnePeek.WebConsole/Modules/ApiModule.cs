97ec658db81d1ed7332a12cc455b630baadc9331
Tobias Klika <cee@live.at>
2015-05-05 01:22:58 +0200

    ability to save spotlight data for current date

--- src/OnePeek.WebConsole/Modules/ApiModule.cs
+++ src/OnePeek.WebConsole/Modules/ApiModule.cs
@@ -1,14 +1,19 @@
 ﻿using Nancy;
+using Newtonsoft.Json;
 using OnePeek.Api;
 using OnePeek.Entities;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.IO;
+using System.Reflection;
+using System.Threading;
 
 namespace OnePeek.WebConsole.Modules
 {
   public class ApiModule : NancyModule
   {
-    public ApiModule() : base("api/")
+    public ApiModule(IRootPathProvider rootProvider) : base("api/")
     {
       OnePeekApi api = new OnePeekApi();
 
@@ -16,6 +21,51 @@ namespace OnePeek.WebConsole.Modules
       {
         AppMetadata meta = await api.GetMetadata(ctx.id, StoreType.WindowsPhone8, StoreCultureType.EN_US);
         return Response.AsJson(meta);
+      };
+
+
+      Post["/spotlight", true] = async (ctx, token) =>
+      {
+        IEnumerable<StoreSpotlightIdResults> apps1 = await api.GetSpotlightIdsForAllCultures(StoreSpotlightType.Apps, StoreType.WindowsPhone8, default(CancellationToken));
+        IEnumerable<StoreSpotlightIdResults> games1 = await api.GetSpotlightIdsForAllCultures(StoreSpotlightType.Games, StoreType.WindowsPhone8, default(CancellationToken));
+
+        Dictionary<string, string> apps = new Dictionary<string, string>();
+        Dictionary<string, string> games = new Dictionary<string, string>();
+
+        foreach (var result in apps1)
+        {
+          foreach (var id in result.Ids)
+          {
+            if (!apps.ContainsKey(id))
+            {
+              apps.Add(id, result.StoreCultureType.ToString());
+            }
+            else
+            {
+              apps[id] = apps[id] + "," + result.StoreCultureType.ToString();
+            }
+          }
+        }
+
+        foreach (var result in games1)
+        {
+          foreach (var id in result.Ids)
+          {
+            if (!games.ContainsKey(id))
+            {
+              games.Add(id, result.StoreCultureType.ToString());
+            }
+            else
+            {
+              games[id] = games[id] + "," + result.StoreCultureType.ToString();
+            }
+          }
+        }
+
+        File.WriteAllText(Path.Combine(rootProvider.GetRootPath(), "AppData", "apps-" + DateTime.Now.ToString("yyyy-MM-dd") + ".json"), JsonConvert.SerializeObject(apps));
+        File.WriteAllText(Path.Combine(rootProvider.GetRootPath(), "AppData", "games-" + DateTime.Now.ToString("yyyy-MM-dd") + ".json"), JsonConvert.SerializeObject(games));
+
+        return Response.AsText("ok");
       };
 
 
